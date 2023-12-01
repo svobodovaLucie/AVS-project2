2991683d4dc14bffb287c8ab21772caa83770159
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2023-12-01 17:26:23 +0100

    [loop] parallel version added

--- parallel_builder/loop_mesh_builder.cpp
+++ parallel_builder/loop_mesh_builder.cpp
@@ -1,11 +1,11 @@
 /**
  * @file    loop_mesh_builder.cpp
  *
- * @author  FULL NAME <xlogin00@stud.fit.vutbr.cz>
+ * @author  Lucie Svobodová <xsvobo1x@stud.fit.vutbr.cz>
  *
  * @brief   Parallel Marching Cubes implementation using OpenMP loops
  *
- * @date    DATE
+ * @date    30 November 2023
  **/
 
 #include <iostream>
@@ -22,15 +22,64 @@ LoopMeshBuilder::LoopMeshBuilder(unsigned gridEdgeSize)
 
 unsigned LoopMeshBuilder::marchCubes(const ParametricScalarField &field)
 {
-    return 0;
+    // 1. Compute total number of cubes in the grid.
+    size_t totalCubesCount = mGridSize*mGridSize*mGridSize;
+
+    unsigned totalTriangles = 0;
+
+    // 2. Loop over each coordinate in the 3D grid.
+    #pragma omp parallel for schedule(static) reduction(+:totalTriangles)
+    for(size_t i = 0; i < totalCubesCount; ++i)
+    {
+        // 3. Compute 3D position in the grid.
+        Vec3_t<float> cubeOffset( i % mGridSize,
+                                 (i / mGridSize) % mGridSize,
+                                  i / (mGridSize*mGridSize));
+
+        // 4. Evaluate "Marching Cube" at given position in the grid and
+        //    store the number of triangles generated.
+        unsigned buildCubeRes = buildCube(cubeOffset, field);
+        #pragma omp critical
+        totalTriangles += buildCubeRes;
+    }
+
+    // 5. Return total number of triangles generated.
+    return totalTriangles;
 }
 
+// method called from buildCube()
 float LoopMeshBuilder::evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field)
 {
-    return 0.0f;
+    // 1. Store pointer to and number of 3D points in the field
+    //    (to avoid "data()" and "size()" call in the loop).
+    const Vec3_t<float> *pPoints = field.getPoints().data();
+    const unsigned count = unsigned(field.getPoints().size());
+
+    float value = std::numeric_limits<float>::max();
+
+    // 2. Find minimum square distance from points "pos" to any point in the
+    //    field.
+    for(unsigned i = 0; i < count; ++i)
+    {
+        float distanceSquared  = (pos.x - pPoints[i].x) * (pos.x - pPoints[i].x);
+        distanceSquared       += (pos.y - pPoints[i].y) * (pos.y - pPoints[i].y);
+        distanceSquared       += (pos.z - pPoints[i].z) * (pos.z - pPoints[i].z);
+
+        // Comparing squares instead of real distance to avoid unnecessary
+        // "sqrt"s in the loop.
+        value = std::min(value, distanceSquared);
+    }
+
+    // 3. Finally take square root of the minimal square distance to get the real distance
+    return sqrt(value);
 }
 
+// method called from buildCube()
 void LoopMeshBuilder::emitTriangle(const BaseMeshBuilder::Triangle_t &triangle)
 {
-
+    // Store generated triangle into vector (array) of generated triangles.
+    // The pointer to data in this array is return by "getTrianglesArray(...)" call
+    // after "marchCubes(...)" call ends.
+    #pragma omp critical
+    mTriangles.push_back(triangle);
 }
--- parallel_builder/loop_mesh_builder.h
+++ parallel_builder/loop_mesh_builder.h
@@ -1,11 +1,11 @@
 /**
  * @file    loop_mesh_builder.h
  *
- * @author  FULL NAME <xlogin00@stud.fit.vutbr.cz>
+ * @author  Lucie Svobodová <xsvobo1x@stud.fit.vutbr.cz>
  *
  * @brief   Parallel Marching Cubes implementation using OpenMP loops
  *
- * @date    DATE
+ * @date    30 November 2023
  **/
 
 #ifndef LOOP_MESH_BUILDER_H
@@ -23,7 +23,8 @@ protected:
     unsigned marchCubes(const ParametricScalarField &field);
     float evaluateFieldAt(const Vec3_t<float> &pos, const ParametricScalarField &field);
     void emitTriangle(const Triangle_t &triangle);
-    const Triangle_t *getTrianglesArray() const { return nullptr; }
+    const Triangle_t *getTrianglesArray() const { return mTriangles.data(); }
+    std::vector<Triangle_t> mTriangles; ///< Temporary array of triangles
 };
 
 #endif // LOOP_MESH_BUILDER_H
