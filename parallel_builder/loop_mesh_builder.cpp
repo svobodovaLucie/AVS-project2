a404ba0e10dc6640be00354a76d3d13944ff6d08
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2023-12-01 18:55:07 +0100

    [loop] pragma edited

--- parallel_builder/loop_mesh_builder.cpp
+++ parallel_builder/loop_mesh_builder.cpp
@@ -28,7 +28,7 @@ unsigned LoopMeshBuilder::marchCubes(const ParametricScalarField &field)
     unsigned totalTriangles = 0;
 
     // 2. Loop over each coordinate in the 3D grid.
-    #pragma omp parallel for schedule(static) reduction(+:totalTriangles)
+    #pragma omp parallel for schedule(static) reduction(+:totalTriangles) shared(field, totalCubesCount)
     for(size_t i = 0; i < totalCubesCount; ++i)
     {
         // 3. Compute 3D position in the grid.
